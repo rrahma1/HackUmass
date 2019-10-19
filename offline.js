8119e6b6afb028b035536fb68f505424ae80280f
rrahma1 <rrahma1@gmail.com>
2019-10-19 16:43:18 -0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1571517529,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/bottom-sheet0.png",
+		"images/enemy0-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/ebullet-sheet0.png",
+		"images/sbullet-sheet0.png",
+		"images/sheild-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
